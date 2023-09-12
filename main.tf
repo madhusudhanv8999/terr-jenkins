65095dc679fee7d7f9da183590fca3cecc57702f
madhusudhanv8999 <123716412+madhusudhanv8999@users.noreply.github.com>
2023-09-12 08:26:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_version = ">= 0.13"
+  required_providers {
+    archive = {
+      source = "hashicorp/archive"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+    template = {
+      source = "hashicorp/template"
+    }
+  }
+}
+
 provider "aws" {
 region = "us-east-1"
 access_key = "AKIASUU6XVLWRG7BG5OI"
@@ -5,7 +20,7 @@ secret_key = "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"
 }
 
 resource "aws_instance" "one" {
-  ami             = "ami-0d81306eddc614a45"
+  ami             = "ami-01c647eace872fc02"
   instance_type   = "t2.micro"
   key_name        = "madhu"
   tags = {
