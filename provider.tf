2c2f00b99ee8d3480a61320efcf4a11cb92e5629
madhusudhanv8999 <123716412+madhusudhanv8999@users.noreply.github.com>
2023-09-12 09:52:04 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,20 +1,7 @@
-terraform {
-  required_version = ">= 0.13"
-  required_providers {
-    archive = {
-      source = "hashicorp/archive"
-    }
-    aws = {
-      source = "hashicorp/aws"
-    }
-    template = {
-      source = "hashicorp/template"
-    }
-  }
-}
-
 provider "aws" {
+ version = "~> 3.0"
 region = "us-east-1"
 access_key = "AKIASUU6XVLWRG7BG5OI"
 secret_key = "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"
 }
+
